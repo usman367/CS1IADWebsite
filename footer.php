2c3a72c192aed0d4d3b9be53120d6336204e649a
usman367 <usmansha367@gmail.com>
2021-03-14 18:56:11 +0000

    Footer added

---
+++ footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css"
+  rel="stylesheet"
+/>
+</head>
+<body>
+
+    <footer class="page-footer bg-dark">
+
+        <div >
+            <div class="container">
+                <div class="row py-4 d-flex align-items-center">
+
+                    <div class="col-md-12 text-center">
+                        <a href="https://www.facebook.com/astonuniversity/"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/AstonUniversity?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com/school/aston-university/"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container text-center text-md-left mt-5 bg-dark">
+            <div class="row">
+
+                <div class="col-md-3 mx-auto mb-4">
+                    <h6 class="text-uppercase font-weight-bold">About Us</h6>
+                    <!-- For the line under the title !-->
+                    <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 125px; height: 2px">
+                    <p class="mt-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </div>
+
+            <div class="col-md-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">Location</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 85px; height: 2px">
+
+                <ul class="list-unstyled">
+                    <li class="my-2"><p>Country: United Kingdom</p></li>
+                    <li class="my-2"><p>City: Birmingham</p></li>
+                    <li class="my-2"><p>Place: Aston University</p></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 mx-auto mb-4">
+                <h6 class="text-uppercase font-weight-bold">Other Links</h6>
+                <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 85px; height: 2px">
+
+                <ul class="list-unstyled">
+                    <li class="my-2"><a href="#">gvvg</a></li>
+                    <li class="my-2"><a href="#">gvvg</a></li>
+                    <li class="my-2"><a href="#">gvvg</a></li>
+                </ul>
+            </div>
+
+            </div>
+        </div>
+
+
+
+        </footer>
+
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"
+></script>
+</body>
+</html>
