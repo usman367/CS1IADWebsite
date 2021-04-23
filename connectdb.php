655c797d18a0f6573c7280ad106a84771669c81d
usman367 <usmansha367@gmail.com>
2021-04-23 17:14:45 +0100

    Final

--- connectdb.php
+++ connectdb.php
@@ -1,9 +1,9 @@
 <?php
 
     $db_host = 'localhost';
-    $db_name = 'astonevents';
-    $username = 'root';
-    $password = '';
+    $db_name = 'u_200091213_db';
+    $username = 'u-200091213';
+    $password = 'dERNr1ysRACLeER';
 
     try{
         $db = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
